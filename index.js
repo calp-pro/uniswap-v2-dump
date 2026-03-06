2f288e86cb9edd1c02cb141bb34c6d4432fe14be
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-03-07 02:08:54 +0700

    ensure with absolute path

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const { load } = require('uniswap-v2-loader')
-const fs = require('fs')
-
-const dump = fs.readFileSync('dump.csv').toString().trim().split('\n')
-console.log('dump.length', dump.length)
 
 module.exports = (params = {}) => load({
     factory: '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f',
-    filename: 'dump.csv',
+    filename: require('path').join(__dirname, 'dump.csv'),
     ...params
 })
