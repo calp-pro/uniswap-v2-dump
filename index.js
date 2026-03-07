be61e97c069898603b11eab932dee7af6fe57008
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-03-07 18:14:39 +0700

    fix index.js syntax "}" typo

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const loader = require('uniswap-v2-loader')
+const dump_csv = require('path').join(__dirname, 'dump.csv')
 
 module.exports.load = (params = {}) => loader.load({
     factory: '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f',
-    filename: require('path').join(__dirname, 'dump.csv'),
+    filename: dump_csv,
     ...params
 })
 
@@ -10,7 +11,7 @@ module.exports.subscribe = (callback, params = {}) => loader.subscribe(
     callback,
     {
         factory: '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f',
-        filename: require('path').join(__dirname, 'dump.csv'),
+        filename: dump_csv,
         ...params
     }
-})
+)
